faee1b661a85e9dfef1aa0e81e5dab48f242c315
James Hobson <james@hobson.space>
2024-07-16 14:58:57 +0200

    Finished QAbstractTableModel

--- src/Graphics/UI/Qml/Internal/QAbstractTableModel.hs
+++ src/Graphics/UI/Qml/Internal/QAbstractTableModel.hs
@@ -7,3 +7,13 @@ import Graphics.UI.Qml.Internal.Types
 
 foreign import ccall "dos_qabstracttablemodel_qmetaobject"
     qMetaObject :: IO (Ptr DosQMetaObject)
+
+
+foreign import ccall "dos_qabstracttablemodel_create"
+    create :: Ptr () -> Ptr DosQMetaObject -> DObjectCallback -> Ptr DosQAbstractItemModelCallbacks -> IO (Ptr DosQAbstractTableModel)
+
+foreign import ccall "dos_qabstracttablemodel_index"
+    index :: Ptr DosQAbstractTableModel -> CInt -> CInt -> Ptr DosQModelIndex -> IO (Ptr DosQModelIndex)
+
+foreign import ccall "dos_qabstracttablemodel_parent"
+    parent :: Ptr DosQAbstractTableModel -> Ptr DosQModelIndex -> IO (Ptr DosQModelIndex)
